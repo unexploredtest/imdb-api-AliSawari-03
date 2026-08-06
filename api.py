2efef1ff61a59cc26992b85166ae700d8f54f9b9
unexploredtest <unexploredtest@tutanota.com>
2026-08-06 12:54:35 +0330

    Make API accept request of any host

--- api.py
+++ api.py
@@ -33,4 +33,4 @@ def get_same_director_writer_alive():
     })
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
